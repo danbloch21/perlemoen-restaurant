ba36afc293123256bb7a644cb27c9c1f6be5ca1f
danbloch21~ <73940682+danbloch21@users.noreply.github.com>
2023-02-08 12:10:26 +0200

    Updates

---
+++ precache-manifest.173f734b27116817aeee6bbf97488438.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "84ecfe6e341ecf8774c2",
+    "url": "/perlemoen-restaurant/static/css/main.7cd27901.chunk.css"
+  },
+  {
+    "revision": "84ecfe6e341ecf8774c2",
+    "url": "/perlemoen-restaurant/static/js/main.84ecfe6e.chunk.js"
+  },
+  {
+    "revision": "1ab1c837dd1c26ad6457",
+    "url": "/perlemoen-restaurant/static/js/1.1ab1c837.chunk.js"
+  },
+  {
+    "revision": "d649eb7952d866e28e50",
+    "url": "/perlemoen-restaurant/static/js/runtime~main.d649eb79.js"
+  },
+  {
+    "revision": "be2151f4e9af529f67ce80896ed8374a",
+    "url": "/perlemoen-restaurant/static/media/meal.be2151f4.mp4"
+  },
+  {
+    "revision": "d719065bb2726afe77e90cec8ea81d5a",
+    "url": "/perlemoen-restaurant/static/media/bg.d719065b.png"
+  },
+  {
+    "revision": "85d27f24ca6480fa5e10c95311f10e19",
+    "url": "/perlemoen-restaurant/static/media/chef.85d27f24.png"
+  },
+  {
+    "revision": "c7fdecee851896549a8f719d1d792e75",
+    "url": "/perlemoen-restaurant/static/media/G.c7fdecee.png"
+  },
+  {
+    "revision": "5a3891e8d95239f82c09715ae11941b6",
+    "url": "/perlemoen-restaurant/static/media/gallery01.5a3891e8.png"
+  },
+  {
+    "revision": "2dd1c3366e4ba309e1ab5ef1c4d31f2d",
+    "url": "/perlemoen-restaurant/static/media/gallery02.2dd1c336.png"
+  },
+  {
+    "revision": "3706b6f7f3577e20cbc4bec6e7698eb5",
+    "url": "/perlemoen-restaurant/static/media/gallery03.3706b6f7.png"
+  },
+  {
+    "revision": "f51d9759a0055fffba51cb9949949771",
+    "url": "/perlemoen-restaurant/static/media/gallery04.f51d9759.png"
+  },
+  {
+    "revision": "e96f9bd45b8e0b34eeb03e93f547ccf8",
+    "url": "/perlemoen-restaurant/static/media/knife.e96f9bd4.png"
+  },
+  {
+    "revision": "5c00c519489e1238ee18cc42a47f141a",
+    "url": "/perlemoen-restaurant/static/media/logo.5c00c519.png"
+  },
+  {
+    "revision": "cc54616224f2b073f9400318c172d8a6",
+    "url": "/perlemoen-restaurant/static/media/menu.cc546162.png"
+  },
+  {
+    "revision": "179769474d7506d29c78a6bec834b00c",
+    "url": "/perlemoen-restaurant/static/media/overlaybg.17976947.png"
+  },
+  {
+    "revision": "72422a1acc4f48e3d82ae3bfa6d1b9d8",
+    "url": "/perlemoen-restaurant/static/media/spoon.72422a1a.svg"
+  },
+  {
+    "revision": "3b8c5febf9880dbe7f002bf3572e713c",
+    "url": "/perlemoen-restaurant/static/media/welcome.3b8c5feb.png"
+  },
+  {
+    "revision": "03ef4140ada413449ce1d79046217392",
+    "url": "/perlemoen-restaurant/static/media/findus.03ef4140.png"
+  },
+  {
+    "revision": "c2d7d58a629c456aac6406b8986272fb",
+    "url": "/perlemoen-restaurant/static/media/laurels.c2d7d58a.png"
+  },
+  {
+    "revision": "a807ceab88095e0cbece581d1600f52d",
+    "url": "/perlemoen-restaurant/static/media/award01.a807ceab.png"
+  },
+  {
+    "revision": "ff49ca57a8d44a4596c62d4966eb5788",
+    "url": "/perlemoen-restaurant/static/media/award02.ff49ca57.png"
+  },
+  {
+    "revision": "73a36a9eac74bab796a0e1c799a7ad4a",
+    "url": "/perlemoen-restaurant/static/media/award03.73a36a9e.png"
+  },
+  {
+    "revision": "d13582bfb80458cf3abd2268d5b5b581",
+    "url": "/perlemoen-restaurant/static/media/award05.d13582bf.png"
+  },
+  {
+    "revision": "f1363e1deeb3ee759b3a365913a97c85",
+    "url": "/perlemoen-restaurant/static/media/sign.f1363e1d.png"
+  },
+  {
+    "revision": "416ffe8d7d8156f426d612f170b79788",
+    "url": "/perlemoen-restaurant/static/media/Shells233pc.416ffe8d.png"
+  },
+  {
+    "revision": "585d31d4497b82cc686e530da42d43cd",
+    "url": "/perlemoen-restaurant/static/media/P.585d31d4.png"
+  },
+  {
+    "revision": "d668a840e16bd3214a7e86d927e1f4da",
+    "url": "/perlemoen-restaurant/index.html"
+  }
+];
